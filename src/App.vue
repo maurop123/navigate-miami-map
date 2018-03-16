da3a89422cfafd7baec086182c1346b32a2fc2cc
mauro <maurop123@gmail.com>
2018-03-16 17:53:15 -0300

    add leaflet map and navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      disable-resize-watcher
+      absolute
+      app
+      class="above-map"
+    >
+      <v-list>
+        <v-list-group
+          v-for="category in categories"
+          :key="category.name"
+          :prepend-icon="category.icon"
+          no-action
+        >
+          <v-list-tile slot="activator">
+            <v-list-tile-content>
+              {{ category.name }}
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile
+            v-for="item in category.items"
+            :key="item.name"
+          >
+            <v-list-tile-content>
+              <v-list-tile-title>
+                {{ item.name }}
+              </v-list-tile-title>
+              <!-- <v&#45;list&#45;tile&#45;subtitle> -->
+              <!--   120 SW 8th St, Miami, FL 33130, USA -->
+              <!-- </v&#45;list&#45;tile&#45;subtitle> -->
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app color="white">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-spacer />
+      <v-toolbar-title>
+        Navigate Miami
+      </v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <leaflet-map />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import LeafletMap from './components/LeafletMap.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      LeafletMap,
+    },
+    data() {
+      return {
+        drawer: false,
+        categories: [{
+          name: 'Education',
+          items: [{
+            name: 'Ironhack',
+          }],
+          icon: 'school',
+        }, {
+          name: 'Accelerator',
+          items: [{
+            'name': 'Watsco Ventures',
+          }],
+          icon: 'directions_run',
+        }],
+      }
+    },
   }
-}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="scss">
+  .above-map {
+    z-index: 2000;
+  }
 </style>
---
+++ src/components/LeafletMap.vue
@@ -0,0 +1,23 @@
+<template>
+  <div ref="map" style="height:100%"></div>
+</template>
+
+<script>
+  import "leaflet/dist/leaflet.css";
+  import L from 'leaflet'
+
+  export default {
+    name: 'leaflet-map',
+    data() {
+      return {
+        map: null,
+      }
+    },
+    mounted() {
+      this.map = L.map(this.$refs.map).setView([25.766, -80.195], 13)
+      L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
+      }).addTo(this.map)
+    },
+  }
+</script>
